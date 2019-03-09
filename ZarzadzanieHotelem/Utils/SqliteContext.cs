c04e762ebd8cd5dd1e3cd2f7b2ed311da002608b
DawidKulinski <dawid.kulinski@outlook.com>
2019-03-09 14:14:45 +0100

    Init DbContext

---
+++ ZarzadzanieHotelem/Utils/SqliteContext.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ZarzadzanieHotelem.Models;
+
+
+namespace ZarzadzanieHotelem.Utils
+{
+    public class SqliteContext : DbContext
+    {
+        //for later usage
+        //private readonly string ConnectionString = ConfigurationManager.AppSettings["ConnectionString"];
+
+        public SqliteContext(string connString):
+            base(connString)
+        {
+            Database.ExecuteSqlCommand("PRAGMA journal_mode=WAL;");
+        }
+
+
+
+        public DbSet<Cleaning> Cleanings { get; set; }
+        public DbSet<Customer> Customers { get; set; }
+        public DbSet<Equipment> Equipments { get; set; }
+        public DbSet<EquipmentPerRoom> EquipmentPerRooms { get; set; }
+        public DbSet<ParkingSlot> ParkingSlots { get; set; }
+        public DbSet<Reservation> Reservations { get; set; }
+        public DbSet<Room> Rooms { get; set; }
+        public DbSet<Worker> Workers { get; set; }
+
+    }
+}
