acb8f01532acaf63fa5bfe5b2b16374d1c5bcc06
DawidKulinski <dawid.kulinski@outlook.com>
2019-03-15 16:55:08 +0100

    Return reservations

---
+++ ZarzadzanieHotelem/Utils/RoomScheduler.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ZarzadzanieHotelem.Utils
+{
+    public static class RoomScheduler
+    {
+        public static IEnumerable<Tuple<DateTime, DateTime>> GetReservations(int roomId)
+        {
+            using (var DbContext = new SqliteContext())
+            {
+                return DbContext.Reservations.Where(x => x.IdRoom == roomId)
+                    .Select(x => new Tuple<DateTime, DateTime>(x.StartTime, x.StopTime));
+            }
+        }
+    }
+}
