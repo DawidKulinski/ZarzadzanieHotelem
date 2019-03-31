3da904d7a21e25378a936d4b95ce925803a4057d
unknown <dawid.kulinski@outlook.com>
2019-03-31 12:22:49 +0200

    Equipment

---
+++ ZarzadzanieHotelem/Utils/ClassUtils.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ZarzadzanieHotelem.Utils
+{
+    static class ClassUtils
+    {
+        public static T EditElement<T>(T modifiedElement, T element) where T: class
+        {
+            foreach(var elem in typeof(T).GetProperties())
+            {
+                if(elem.Name != "Id")
+                   elem.SetValue(modifiedElement, elem.GetValue(element, null));
+            }
+            return modifiedElement;
+        }
+    }
+}
